276338fa71676cf5865096e94bebf88b8b2228f0
GuyKopel1568 <guykopel1568@gmail.com>
2025-02-18 10:06:34 +0200

    Start useReduser on the quiz

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+
+import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questions: [],
+  status: "Loading"
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecived":
+      return { ...state, questions: action.payload, status: "Ready" };
+    default:
+      throw new Error("Active unknown");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function() {
+    fetch("http://localhost:5000/questions")
+      .then(res => res.json())
+      .then(data => dispatch({ type: "dataRecived", payload: data }))
+      .catch(res => console.error("Error"));
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
